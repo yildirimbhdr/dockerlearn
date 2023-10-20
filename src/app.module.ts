7af850d603012a5d0ac1404875ee14866816805b
yildirimbhdr <yildirim.bahadir02@gmail.com>
2023-10-20 03:02:26 +0300

    Db bağlantı test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(
-
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.HOST,
+    port: parseInt(process.env.PORT),
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    entities: [],
+    synchronize: true,
+  },
   ),],
   controllers: [AppController],
   providers: [AppService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! 2' + process.env.PMA_HOST;
+    return 'Hello World!' + process.env.HOST;
   }
 }
